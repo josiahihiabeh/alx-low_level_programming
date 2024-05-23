ffc997b5a662ef6ae082c7b371dba61daa3163b4
Josiah O Ihiabeh <ihiabehojodalejosiah@gmail.com>
2024-05-23 03:32:39 +0100

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char josi;
+
+	for (josi = 'a'; josi <= 'z'; josi++)
+	{
+		putchar(josi);
+	}
+	putchar('\n');
+	return (0);
+}
