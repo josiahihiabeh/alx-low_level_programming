6173d64957fce4cf128b1d585b88cadc2dadd7cd
Josiah O Ihiabeh <ihiabehojodalejosiah@gmail.com>
2024-05-23 03:45:26 +0100

    Update 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Josiah
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b < 58; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
