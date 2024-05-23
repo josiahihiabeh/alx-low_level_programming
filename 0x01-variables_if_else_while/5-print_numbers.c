8233c4da610c43dd5e80597d8b5875f04c97dbd4
Josiah O Ihiabeh <ihiabehojodalejosiah@gmail.com>
2024-05-23 03:42:24 +0100

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Jsiah
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
